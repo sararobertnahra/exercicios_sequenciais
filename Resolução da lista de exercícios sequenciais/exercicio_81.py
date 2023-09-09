49adf348a4e984b4fd0b40e0afab30db484cabc0
Sara Nahra <sara.nahra@fatec.sp.gov.br>
2023-09-09 16:20:29 -0300

    Update exercicio_81.py

--- Resolução da lista de exercícios sequenciais/exercicio_81.py
+++ Resolução da lista de exercícios sequenciais/exercicio_81.py
@@ -1,17 +1,17 @@
 conta = int(input("\nDigite conta de tres digitos: "))
 
-d1 = conta / 100
-d2 = conta % 100 / 10
-d3 = conta % 100 % 10
+d1 = int(conta / 100)
+d2 = int((conta % 100) / 10)
+d3 = int((conta % 100) % 10)
 
-inv = d3 * 100 + d2 * 10 + d1
-soma = conta + inv
+inv = int(d3 * 100 + d2 * 10 + d1)
+soma = int(conta + inv)
 
-d1 = (soma / 100) * 1
-d2 = (soma % 100 / 10) * 2
-d3 = (soma % 100 % 10) * 3
+d1 = int(soma / 100) * 1
+d2 = int(soma % 100 / 10) * 2
+d3 = int(soma % 100 % 10) * 3
 
-digito = (d1 + d2 + d3) % 10
+digito = int(d1 + d2 + d3)
 
 print(f"\ndigito verificador: {digito}")
 print("\n")
