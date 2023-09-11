5ac94175ef368adcae1964c1bdb946c5b250961a
Sara Nahra <sara.nahra@fatec.sp.gov.br>
2023-09-10 23:00:13 -0300

    Update exercicio_57.py

--- Resolução da lista de exercícios sequenciais/exercicio_57.py
+++ Resolução da lista de exercícios sequenciais/exercicio_57.py
@@ -1,6 +1,8 @@
+import math as m
+
 pr1 = float(input("\ndigite pr1: "))
 pr2 = float(input("\ndigite pr2: "))
-mf = (pr1 + pr2) / 2
+mf = (pr1 + pr2) / 2.0
 print(f"\nmedia truncada = {int((mf - 0.5) + 0.001)}")
-print(f"\nmedia arredondada = {int(mf + 0.001)}")
+print(f"\nmedia arredondada = {m.ceil(mf + 0.001)}")
 print("\n")
